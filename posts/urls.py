c26463c94ed27b7a6172060ae4ea0203a5834c27
Adonias <adoniashaile1@gmail.com>
2024-01-31 09:59:23 +0300

    class based apiviews

--- posts/urls.py
+++ posts/urls.py
@@ -3,9 +3,12 @@ from django.urls import path, include
 
 urlpatterns = [
     path("homepage/", views.homepage, name="homepage"),
-    path("", views.allPosts, name="posts"),
-    path("<int:post_id>", views.post_detail, name="post_detail"),
+    path("", views.PostListCreateView.as_view(), name="create_delete_post"),
+    path(
+        "<int:post_id>/",
+        views.UpdateDeleteDetailView.as_view(),
+        name="post_delete_update_detail",
+    ),
     path("update/<int:post_id>", views.update_post, name="update_post"),
     path("delete/<int:post_id>", views.delete_post, name="delete_post"),
 ]
-
--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,66 @@
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, APIView
 
 from .models import PostModel
 from .serializers import PostModelSerializer
 from django.shortcuts import get_object_or_404
+
+
+# create class based apiviews for create and list posts
+class PostListCreateView(APIView):
+    def post(self, request: Request):
+        data = request.data
+        serializer = PostModelSerializer(data=data)
+        print("serializer:", serializer)
+        if serializer.is_valid():
+            serializer.save()
+            response = {
+                "message": "post created",
+                "data": serializer.data,
+            }
+            return Response(data=response, status=status.HTTP_201_CREATED)
+
+    def get(self, request: Request):
+        posts = PostModel.objects.all()
+        serializer = PostModelSerializer(instance=posts, many=True)
+        print("serializer:", serializer)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class UpdateDeleteDetailView(APIView):
+    def put(self, request: Request, post_id: int):
+        post = get_object_or_404(PostModel, pk=post_id)
+
+        data = request.data
+        print("data------------", data)
+        print("post------------", post)
+        serializer = PostModelSerializer(instance=post, data=data)
+        print("serializer:", serializer)
+        if serializer.is_valid():
+            serializer.save()
+            response = {
+                "message": "post updated successfully",
+                "data": serializer.data,
+            }
+            return Response(data=response, status=status.HTTP_200_OK)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request: Request, post_id: int):
+        post = get_object_or_404(PostModel, pk=post_id)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def get(self, request: Request, post_id: int):
+        post = get_object_or_404(PostModel, pk=post_id)
+
+        serializer = PostModelSerializer(instance=post)
+        response = {
+            "message": "post",
+            "data": serializer.data,
+        }
+        return Response(data=response, status=status.HTTP_200_OK)
 
 
 # Create your views here.
